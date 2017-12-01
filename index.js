2625e6a0cd41ab7ecc71802b3b1c98a57f2cbe41
Jannis R <mail@jannisr.de>
2017-12-01 15:44:22 +0100

    wrap in RSS

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-// todo
+const Feed = require('rss')
+
+const generateRawFeed = require('./raw')
+
+const generateRSSFeed = (serie) => {
+	return generateRawFeed(serie)
+	.then(({meta, items}) => {
+		const feed = new Feed(meta)
+		for (let item of items) feed.item(item)
+		return feed.xml()
+	})
+}
+
+module.exports = generateRSSFeed
